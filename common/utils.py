b7fe876617905a2ee8734aafaa21752d295dbac9
Vladimir Moskva <m@fulc.ru>
2012-01-18 19:28:58 +0400

    Refactored runner

---
+++ common/utils.py
@@ -0,0 +1,15 @@
+import sys
+
+def get_problem_id():
+    if len(sys.argv) > 1:
+        return sys.argv[1]
+    else:
+        return raw_input('Enter the problem id: ')
+
+def import_problem(problem_id):
+    try:
+        problem = __import__('problems.%s' % problem_id, globals(), locals(), ['solver', 'fetcher'], -1)
+        return problem
+    except ImportError:
+        print 'Failed to import the module "%s". It\'s either missing or invalid' % problem_id
+        sys.exit(1)
--- runner.py
+++ runner.py
@@ -1,24 +1,10 @@
-import sys
-
+from common.utils import get_problem_id, import_problem
 from common.io import output
-
-def get_problem_id():
-    if len(sys.argv) > 1:
-        return sys.argv[1]
-    else:
-        return raw_input('Enter the problem id: ')
 
 def main():
     problem_id = get_problem_id()
-    try:
-        problem = __import__('problems.practice.%s' % problem_id, globals(), locals(), ['solver', 'fetcher'], -1)
-    except ImportError:
-        print 'Failed to import the module "%s". It\'s either missing or invalid' % problem_id
-        return 1
-
+    problem = import_problem(problem_id)
     output(problem_id, problem.fetcher, problem.solver)
-    return 0
 
 if __name__ == '__main__':
     exit_code = main()
-    sys.exit(exit_code)
